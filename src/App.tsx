c3741716c6230ef96d1ad306c10fb8c7b4a8dd75
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-12 09:43:54 -0300

    feat(auth): add session helpers and protected attendant route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GetTicket from './screens/GetTicket';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import Attendant from './screens/Attendent';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GetTicket />} />
+        <Route
+          path="/attendent"
+          element={(
+            <ProtectedRoute>
+              <Attendant />
+            </ProtectedRoute>
+          )}
+        />
       </Routes>
     </BrowserRouter>
   );
